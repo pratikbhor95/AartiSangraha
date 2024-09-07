8bebe7a7605be90f9e9c83073a7aa45da3e1f633
pratikbhor95 <pratikbhor95@gmail.com>
2024-09-07 02:35:48 +0100

    first commit

---
+++ serviceworker.js
@@ -0,0 +1,34 @@
+const cacheName = "MyCache_1";
+const precachedResources = ["/", "/index.html", "/EnglishVersion.html"];
+
+async function precache() {
+  const cache = await caches.open(cacheName);
+  return cache.addAll(precachedResources);
+}
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(precache());
+});
+
+function isCacheable(request) {
+  const url = new URL(request.url);
+  return !url.pathname.endsWith(".json");
+}
+
+async function cacheFirstWithRefresh(request) {
+  const fetchResponsePromise = fetch(request).then(async (networkResponse) => {
+    if (networkResponse.ok) {
+      const cache = await caches.open("MyCache_1");
+      cache.put(request, networkResponse.clone());
+    }
+    return networkResponse;
+  });
+
+  return (await caches.match(request)) || (await fetchResponsePromise);
+}
+
+self.addEventListener("fetch", (event) => {
+  if (isCacheable(event.request)) {
+    event.respondWith(cacheFirstWithRefresh(event.request));
+  }
+});
